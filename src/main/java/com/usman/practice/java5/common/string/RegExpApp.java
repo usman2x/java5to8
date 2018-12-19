694bc1e8a9102eadde626cfef2e938027753d7c5
Muhammad Usman <hafizusman530@ymail.com>
2018-12-19 17:09:30 +0500

    start RegExp practice

---
+++ src/main/java/com/usman/practice/java5/common/string/RegExpApp.java
@@ -0,0 +1,25 @@
+package com.usman.practice.java5.common.string;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+public class RegExpApp {
+    public static void main(String[] args) {
+    }
+
+    // TODO public String removeSpaces(String value){}
+
+    // TODO public String replaceSpacesWithCharacter(String value, String toReplaceString){}
+
+    // TODO public String[] searchSpecificPatternStrings(String value, String[] patterns){}
+
+    // TODO public String removeSpaces(String value){}
+
+    // TODO public String replaceIfMoreThanTwoSpaces(String value){}
+
+    // TODO public boolean isAlphaNumericPasswordValid(String value){}
+
+    // CNIC Example 36504-24800411-1
+    // TODO public boolean isValidCNIC(String value){}
+
+
+}
