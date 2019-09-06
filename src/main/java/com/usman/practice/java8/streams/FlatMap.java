60ec270729a953cc01cc3665f6a0af9f3f013108
Muhammad Usman <hafizusmandev@gmail.com>
2019-09-06 18:36:38 +0500

    Add flat map example

---
+++ src/main/java/com/usman/practice/java8/streams/FlatMap.java
@@ -0,0 +1,51 @@
+package com.ccs.mmf;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class FlatMap {
+    public static void main(String[] args) {
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student("Usman", "25"));
+        studentList.add(new Student("Ali", "24"));
+        studentList.add(new Student("Ali 2", "24"));
+
+        Map<String, List<Student>> stringListMap = studentList.stream()
+                .collect(Collectors.groupingBy(Student::getAge, Collectors.toList()));
+
+        System.out.println(stringListMap.entrySet().stream()
+                .filter(stringListEntry -> stringListEntry.getKey().equals("24"))
+                .flatMap(stringListEntry -> stringListEntry.getValue().stream())
+                .filter(student -> student.getName().equals("Ali"))
+                .findFirst().get());
+
+    }
+
+    public static class Student{
+        private String name;
+        private String Age;
+
+        public Student(String name, String age) {
+            this.name = name;
+            Age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getAge() {
+            return Age;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "name='" + name + '\'' +
+                    ", Age='" + Age + '\'' +
+                    '}';
+        }
+    }
+}
